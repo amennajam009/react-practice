87b092c0557afe515ba16344a7f68ea97dbb03e1
amennajam009 <amennajam7@gmail.com>
2024-03-25 21:02:30 +0500

    add

--- src/conditional/student_child.jsx
+++ src/conditional/student_child.jsx
@@ -4,16 +4,29 @@ function Student_child(props) {
     const studentData = props.student_array
     console.log('student Array' , studentData)
   return (
-    <div className='d-flex ; justify-content-center mt-5'>
+    <div>
+        <div className='d-flex ; justify-content-center mt-5'>
       <h1>Student Child Component</h1>
-      <table>
-        <thead>
-            <th>Name</th>
-            <th>Age</th>
-            <th>University</th>
-            <th>Contact_no</th>
-        </thead>
-      </table>
+      </div>
+      <table className="table">
+  <thead>
+    <tr>
+      <th scope="col">Name</th>
+      <th scope="col">Age</th>
+      <th scope="col">University</th>
+      <th scope="col">Contact_no</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>{studentData.name}</td>
+      <td>{studentData.age}</td>
+      <td>{studentData.university}</td>
+      <td>{studentData.contact_no}</td>
+
+    </tr>
+  </tbody>
+</table>
     </div>
   )
 }
