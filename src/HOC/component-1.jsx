06e994c5d63797030095ed0fd320ba015d840c6d
amennajam009 <amennajam7@gmail.com>
2024-05-26 01:05:05 +0500

    ADD

--- src/HOC/component-1.jsx
+++ src/HOC/component-1.jsx
@@ -1,17 +1,3 @@
-// import React, { useState } from 'react'
-// import Component_2 from './component-2'
-// import Counter from './hoc'
-// export default function Component_1() {
-//   return (
-//     <><div className='text-center mt-5'>
-//           <h1>{count}</h1>
-//           <button className='btn btn-success' onClick={increaseNumber}>Click me</button>
-//       </div>
-//       <Component_2 />
-//       </>
-//   )
-// }
-
 import React from 'react'
 import Counter from './hoc'
 import Component_2 from './component-2'
