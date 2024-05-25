c082b689bff410e5bef5992b1e6f193550f20726
amennajam009 <amennajam7@gmail.com>
2024-05-26 01:21:22 +0500

    add

--- src/HOC/hoc.jsx
+++ src/HOC/hoc.jsx
@@ -1,11 +1,11 @@
 import React ,{useState} from 'react'
 
 const Counter = (WrappedComponent) =>{
-    const [count , setCount] = useState(0)
-    function increaseNumber(){
-        setCount(count + 1)
-    }  
     function Hoc() {
+        const [count , setCount] = useState(0)
+        function increaseNumber(){
+            setCount(count + 1)
+        }  
         return (
           <div>
             <WrappedComponent count={count} increment={increaseNumber}/>
