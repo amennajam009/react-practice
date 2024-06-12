ad5376a7a390c72b349c985510e04f68ebe1bcf5
amennajam009 <amennajam7@gmail.com>
2024-06-12 13:05:23 +0500

    sdd

--- src/state-CRUD/state_crud.jsx
+++ src/state-CRUD/state_crud.jsx
@@ -60,6 +60,36 @@ function State_crud() {
 </div>
   <button onClick={submit} class="btn btn-primary">Submit</button>
 </form>
+
+{myArray.map((items)=> (
+<>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">First Name</th>
+      <th scope="col">Last Name</th>
+      <th scope="col">Country</th>
+      <th scope="col">City</th>
+      <th scope="col">Status</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>{items.first_name}</td>
+      <td>{items.last_name}</td>
+      <td>{items.country}</td>
+      <td>{items.city}</td>
+      <td>{items.check_status}</td>
+    </tr>
+  </tbody>
+  <tbody>
+  </tbody>
+</table>
+</>
+))}
+
+
 </div>
 </div>
 </>
